602d6160601386d7744d3f6b8f2210255ee8c2b1
tliboro <tylerliboro@yahoo.com>
2020-02-09 15:51:04 -0500

    loading files

---
+++ Music.R
@@ -0,0 +1,37 @@
+libs <- c('data.table', 'dplyr', 'magrittr', 'ggplot2', 'plyr', 'readr', 'plotly')
+lapply(libs, require, character.only = T)
+
+dir <- "/Users/tylerliboro/Desktop/MyData/"
+
+files <- list.files(path = dir, pattern = "*.csv", all.files = T, full.names = T)   
+
+data <- do.call(rbind, lapply(files, function(fn) read.csv(file = fn, header = T)) ) %>% as.data.table
+
+#Exploration
+head(data)  
+summary(data)
+names(data)
+
+dt <- data[, secPlayed := format( as.POSIXct(Sys.Date())+`msPlayed`/1000, "%M:%S")] %>%
+  .[, minPlayed := as.numeric(substring(text = secPlayed, 
+                                      first = 2, 
+                                      last = 2) )] %>%
+  .[, hrPlayed := round(x = minPlayed/60, digits = 2)] %>%
+  .[, endtime_Day := substr(x = endTime, start = 9, stop = 10)] %>%
+  .[, endtime_Month := substr(x = endTime, start = 6, stop = 7)] %>%
+  .[, endtime_Year := substr(x = endTime, start = 1, stop = 4)] 
+  
+topartists_2019 <- dt[endtime_Year %in% 2019]$artistName %>% table %>%
+  sort(x = ., decreasing = T) %>% as.data.table %>% setnames(., old = '.', new = 'Artist') %>%
+  .[1:10]
+
+newDF <- dt[artistName %in% topartists_2019$Artist]
+newDF <- as.data.frame(newDF)
+newDF$artistName <- factor(newDF$artistName)
+
+p <- plot_ly(data = newDF, x = ~endtime_Month, 
+        color = ~artistName, type = 'box',
+        showlegend = F) %>%
+  config(displayModeBar = F) %>%
+  layout(title = 'Top Artists in 2019')
+
