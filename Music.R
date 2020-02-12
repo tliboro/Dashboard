0a207c858a0524983bc27e6ef51a6f12332dfcb6
tliboro <tylerliboro@yahoo.com>
2020-02-12 18:35:03 -0500

    Minor updates to dashboard include sectioned charts.. working towards crosstalk

--- Music.R
+++ Music.R
@@ -1,4 +1,4 @@
-libs <- c('data.table', 'dplyr', 'magrittr', 'ggplot2', 'plyr', 'readr', 'plotly')
+libs <- c('data.table', 'dplyr', 'magrittr', 'ggplot2', 'plyr', 'readr', 'plotly', 'crosstalk', 'DT')
 lapply(libs, require, character.only = T)
 
 dir <- "/Users/tylerliboro/Desktop/MyData/"
@@ -21,6 +21,9 @@ dt <- data[, secPlayed := format( as.POSIXct(Sys.Date())+`msPlayed`/1000, "%M:%S
   .[, endtime_Month := substr(x = endTime, start = 6, stop = 7)] %>%
   .[, endtime_Year := substr(x = endTime, start = 1, stop = 4)] 
   
+
+shared_dt <- SharedData$new(dt)
+
 topartists_2019 <- dt[endtime_Year %in% 2019]$artistName %>% table %>%
   sort(x = ., decreasing = T) %>% as.data.table %>% setnames(., old = '.', new = 'Artist') %>%
   .[1:10]
@@ -32,6 +35,12 @@ newDF$artistName <- factor(newDF$artistName)
 p <- plot_ly(data = newDF, x = ~endtime_Month, 
         color = ~artistName, type = 'box',
         showlegend = F) %>%
-  config(displayModeBar = F) %>%
-  layout(title = 'Top Artists in 2019')
+  config(displayModeBar = F) #%>%
+  #layout(title = 'Top Artists in 2019')
 
+songs <- dt$trackName %>% table %>% 
+  sort(x = ., decreasing = T) %>% as.data.table %>%
+  setnames(., old = '.', new = 'Song') %>%
+  .[1:10] %>%
+  .[ , Artist := dt[trackName %in% Song]$artistName, by = c("Song")] %>%
+  .[, c('Artist', 'Song', 'N')]
