590348625875baf95d400453665290c6c985c408
tliboro <tylerliboro@yahoo.com>
2020-02-13 23:33:54 -0500

    finally connected the data tables together through crosstalk and an id

--- Music.R
+++ Music.R
@@ -12,6 +12,12 @@ head(data)
 summary(data)
 names(data)
 
+returnDay <- function(day) {
+  list <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+            "Friday", "Saturday")
+  list[day]
+}
+
 dt <- data[, secPlayed := format( as.POSIXct(Sys.Date())+`msPlayed`/1000, "%M:%S")] %>%
   .[, minPlayed := as.numeric(substring(text = secPlayed, 
                                       first = 2, 
@@ -19,10 +25,12 @@ dt <- data[, secPlayed := format( as.POSIXct(Sys.Date())+`msPlayed`/1000, "%M:%S
   .[, hrPlayed := round(x = minPlayed/60, digits = 2)] %>%
   .[, endtime_Day := substr(x = endTime, start = 9, stop = 10)] %>%
   .[, endtime_Month := substr(x = endTime, start = 6, stop = 7)] %>%
-  .[, endtime_Year := substr(x = endTime, start = 1, stop = 4)] 
+  .[, endtime_Year := substr(x = endTime, start = 1, stop = 4)] %>%
+  .[, Day := wday(endTime)] %>%
+  .[, Day := returnDay(Day)]
   
 
-shared_dt <- SharedData$new(dt)
+
 
 topartists_2019 <- dt[endtime_Year %in% 2019]$artistName %>% table %>%
   sort(x = ., decreasing = T) %>% as.data.table %>% setnames(., old = '.', new = 'Artist') %>%
@@ -41,6 +49,28 @@ p <- plot_ly(data = newDF, x = ~endtime_Month,
 songs <- dt$trackName %>% table %>% 
   sort(x = ., decreasing = T) %>% as.data.table %>%
   setnames(., old = '.', new = 'Song') %>%
-  .[1:10] %>%
+  #.[1:10] %>%
   .[ , Artist := dt[trackName %in% Song]$artistName, by = c("Song")] %>%
-  .[, c('Artist', 'Song', 'N')]
+  .[, c('Artist', 'Song', 'N')] 
+  
+
+
+
+#'Crosstalk data table that is connnected to the sidebar
+shared_dt <- SharedData$new(data = dt, key = dt$endtime_Year, group = 'Year')
+
+
+#shared_songs <- SharedData$new(data = songs, key = songs$)
+
+total_minutes <- dt$minPlayed %>% sum
+
+
+library(lubridate)
+
+
+
+best_day <- dt$Day %>% table %>%
+  sort(., decreasing = T) %>% as.data.table %>%
+  setnames(., old = '.', new = 'Day') %>%
+ .[1, 'Day'] %>% as.character
+
