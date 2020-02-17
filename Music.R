c1d6a250c7bb78ea867af8e8bd62ba1a59b94dad
tliboro <tylerliboro@yahoo.com>
2020-02-17 10:35:40 -0500

    Added variables that will hold static info (value boxes) for 2019. Need to Replicate and add a currennt one. Maybe different colors from old and new

--- Music.R
+++ Music.R
@@ -27,7 +27,9 @@ dt <- data[, secPlayed := format( as.POSIXct(Sys.Date())+`msPlayed`/1000, "%M:%S
   .[, endtime_Month := substr(x = endTime, start = 6, stop = 7)] %>%
   .[, endtime_Year := substr(x = endTime, start = 1, stop = 4)] %>%
   .[, Day := wday(endTime)] %>%
-  .[, Day := returnDay(Day)]
+  .[, Day := returnDay(Day)] %>%
+  .[, hour_listened := substr(x = endTime, start = 12, stop = 13) ] %>%
+  .[hour_listened %in% '00', hour_listened := '24'] #Formatting
   
 
 
@@ -62,15 +64,65 @@ shared_dt <- SharedData$new(data = dt, key = dt$endtime_Year, group = 'Year')
 
 #shared_songs <- SharedData$new(data = songs, key = songs$)
 
-total_minutes <- dt$minPlayed %>% sum
+total_minutes <- dt$minPlayed %>% 
+  sum %>% 
+  prettyNum(., big.mark = ',', scientific = F)
 
 
 library(lubridate)
 
+get_ordered <- function(column, name) {
+  column %>% table %>%
+    sort(., decreasing = T) %>% as.data.table(.) %>% 
+    setnames(., old = '.', new = name) #%>%
+  #  .[1, name] %>% as.character(.)
+  
+}
+returnCharacter <- function(object, name) {
+  return(object[1, ..name] %>% as.character(.) )
+}
 
+x <- get_ordered(dt$Day, 'Day')
 
 best_day <- dt$Day %>% table %>%
   sort(., decreasing = T) %>% as.data.table %>%
   setnames(., old = '.', new = 'Day') %>%
  .[1, 'Day'] %>% as.character
+
+
+#'Last Metric Should be the total amount of new songs that were added 
+#'within the past month to actually record my findinds and maybe do an extra analyis
+Sys.time()
+latest.date <- Sys.Date() %>% as.Date
+
+#'Can also try to get what hours of the day i'll listen to the most music
+
+t <- dt$hour_listened %>% table %>%
+  sort(., decreasing = T) %>% as.data.table %>%
+  setnames(., old = '.', new = 'Time')
+t
+
+
+p <- dt[endtime_Year %in% '2020'] %>% .[, c('hour_listened')] %>% table %>%
+  sort(., decreasing = T) %>% as.data.table %>%
+  setnames(., old = '.', new = 'Time')
+p
+
+
+#If SharedData causes errors the just use the 
+
+total_min2019 <- dt[endtime_Year %in% 2019]$minPlayed %>% 
+  sum %>% 
+  prettyNum(., big.mark = ',', scientific = F)
+
+orderDay_2019 <- get_ordered(dt[endtime_Year %in% '2019']$`Day`, 'Day')
+topDay_2019 <- returnCharacter(orderDay_2019, 'Day')
+orderHour_2019 <- get_ordered(dt[endtime_Year %in% '2019']$`hour_listened`, 'hour_listened')
+topHour_2019 <- returnCharacter(orderHour_2019, 'hour_listened')
+
+#'Crosstalk data table that is connnected to the sidebar
+#shared_dt <- SharedData$new(data = dt, key = dt$endtime_Year, group = 'Year')
+
+#MASTER_TABLE <- as.data.table()
+
 
