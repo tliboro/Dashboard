a4779bc1c1e8162afcb4dcbf48163dd152b31230
tliboro <tylerliboro@yahoo.com>
2020-02-19 16:57:08 -0500

    Updated from last coding sesh. Need just in case for version control.

--- Music.R
+++ Music.R
@@ -1,4 +1,6 @@
-libs <- c('data.table', 'dplyr', 'magrittr', 'ggplot2', 'plyr', 'readr', 'plotly', 'crosstalk', 'DT')
+libs <- c('data.table', 'dplyr', 'magrittr', 
+          'ggplot2', 'plyr', 'readr', 'plotly', 
+          'crosstalk', 'DT', 'lubridate')
 lapply(libs, require, character.only = T)
 
 dir <- "/Users/tylerliboro/Desktop/MyData/"
@@ -64,13 +66,6 @@ shared_dt <- SharedData$new(data = dt, key = dt$endtime_Year, group = 'Year')
 
 #shared_songs <- SharedData$new(data = songs, key = songs$)
 
-total_minutes <- dt$minPlayed %>% 
-  sum %>% 
-  prettyNum(., big.mark = ',', scientific = F)
-
-
-library(lubridate)
-
 get_ordered <- function(column, name) {
   column %>% table %>%
     sort(., decreasing = T) %>% as.data.table(.) %>% 
@@ -81,13 +76,6 @@ get_ordered <- function(column, name) {
 returnCharacter <- function(object, name) {
   return(object[1, ..name] %>% as.character(.) )
 }
-
-x <- get_ordered(dt$Day, 'Day')
-
-best_day <- dt$Day %>% table %>%
-  sort(., decreasing = T) %>% as.data.table %>%
-  setnames(., old = '.', new = 'Day') %>%
- .[1, 'Day'] %>% as.character
 
 
 #'Last Metric Should be the total amount of new songs that were added 
@@ -111,6 +99,9 @@ p
 
 #If SharedData causes errors the just use the 
 
+
+
+#2019 Metrics
 total_min2019 <- dt[endtime_Year %in% 2019]$minPlayed %>% 
   sum %>% 
   prettyNum(., big.mark = ',', scientific = F)
@@ -119,6 +110,63 @@ orderDay_2019 <- get_ordered(dt[endtime_Year %in% '2019']$`Day`, 'Day')
 topDay_2019 <- returnCharacter(orderDay_2019, 'Day')
 orderHour_2019 <- get_ordered(dt[endtime_Year %in% '2019']$`hour_listened`, 'hour_listened')
 topHour_2019 <- returnCharacter(orderHour_2019, 'hour_listened')
+
+#2020 Metrics
+total_min2020 <- dt[endtime_Year %in% 2020]$minPlayed %>% 
+  sum %>% 
+  prettyNum(., big.mark = ',', scientific = F)
+
+orderDay_2020 <- get_ordered(dt[endtime_Year %in% '2020']$`Day`, 'Day')
+topDay_2020 <- returnCharacter(orderDay_2020, 'Day')
+orderHour_2020 <- get_ordered(dt[endtime_Year %in% '2020']$`hour_listened`, 'hour_listened')
+topHour_2020 <- returnCharacter(orderHour_2020, 'hour_listened')
+
+years <- c(2019, 2020)
+
+#'The following lapply functions will find the metrics of our interest in different years
+#'(1) Total Minutes is below.
+list_totalMinutes <- lapply(years, function(year) {
+  time <- dt[endtime_Year %in% year]$minPlayed %>%
+    sum %>% prettyNum(., big.mark = ',', scintific = F)
+  output <- c(year, time)
+})
+totalMinutes <- cbind(as.data.table(list_totalMinutes[1]), 
+      as.data.table(list_totalMinutes[2]) ) %>% 
+  setnames(., as.character(.[1,]) ) %>% .[-1,]
+
+
+#'(2) Order Day
+list_weeklyMin <- lapply(years, function(year) {
+  get_ordered(dt[endtime_Year %in% year]$`Day`, 'Day') %>%
+    .[, Year := year]
+})
+weeklyMin <- rbind(as.data.table(list_weeklyMin[1]), 
+                   as.data.table(list_weeklyMin[2]) )
+
+
+list_hour <- lapply(years, function(year) {
+  get_ordered(dt[endtime_Year %in% year]$`hour_listened`, 'hour_listened') %>% as.data.table %>%
+    .[, Year := year]
+})
+yearly_hour <- rbind(as.data.table(list_hour[1]), as.data.table(list_hour[2]) )
+
+
+
+#'(3) Top Day
+list_TopDays <- lapply(years, function(year) {
+  topHour_2020 <- returnCharacter(dt[Year %in% year], 'hour_listened')
+})
+
+
+
+getTopDay <- function(year) {
+  setMax <- weeklyMin
+  wk <- weeklyMin[Year %in% year]
+  topday <- wk[N %in% max(wk$N)]$Day
+  return(topday)
+}
+
+topHour_2020 <- returnCharacter(orderHour_2020, 'hour_listened')
 
 #'Crosstalk data table that is connnected to the sidebar
 #shared_dt <- SharedData$new(data = dt, key = dt$endtime_Year, group = 'Year')
