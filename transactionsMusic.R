0761a5234c459843d0c0830f2d01218cef10a2f7
Tyler Liboro <tylerliboro@Tylers-MacBook-Pro.local>
2020-03-06 06:39:47 -0500

    Proof that there was a significance and the increase was real; however, it did not seem like enough to fully explore without changing bases of either Music or Purchases. Need to narrow down one of the two or each of the variables to be less broad.

--- transactionsMusic.R
+++ transactionsMusic.R
@@ -16,7 +16,7 @@ bank <- read.csv(file = path, stringsAsFactors = F) %>% as.data.table %>%
   .[, Weekday := as.factor(weekdays(Date))]
 
 
-DT_AMEX <- bank[Account.Name %in% 'American Express']
+DT_AMEX <- bank[Account.Name %in% 'American Express'] %>% .[,Purchases := 1]
 ordered_weekly <- c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 
                     'Thursday', 'Friday', 'Saturday')
 DT_AMEX$Weekday <- ordered(x = DT_AMEX$Weekday, c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 
@@ -28,25 +28,25 @@ days <- seq(from =x, to = y , by = 'days') %>%
   as.data.table %>% setnames(., '.', 'days') ; rm(x, y)
 
 #Combining Data Sets (BANK )
-DT.MUSIC.BANK <- dcast(data = DT_MUSIC, formula = Date ~ ., fun.aggregate = sum, 
+DT_MUSIC_BANK <- dcast(data = DT_AMEX, formula = Date ~ ., fun.aggregate = sum, 
                         value.var = c('Amount', 'Purchases') ) %>%
   .[year(Date) %in% 2019] %>% #Only interested in the yearly data. 
         merge(x = ., y = DT_MUSIC, 
             by.x = 'Date', by.y = 'days', all.y = T)
-DT.MUSIC.BANK[is.na(DT.MUSIC.BANK)] <- 0
+DT_MUSIC_BANK[is.na(DT_MUSIC_BANK)] <- 0
 
 
 
 
 # Creating Seasonal Data
-MUSIC_SPRING <- DT.MUSIC.BANK[Date > seasons$Spring[1] & Date < seasons$Spring[2]]
+MUSIC_SPRING <- DT_MUSIC_BANK[Date > seasons$Spring[1] & Date < seasons$Spring[2]]
 summary(MUSIC_SPRING)
 
 seasonal_averages <- do.call(rbind, lapply(names(seasons), function(season) {
           beginning <- seasons[[season]][1]
           end <- seasons[[season]][2]
           
-          avg <- DT.MUSIC.BANK[beginning < Date & Date < end]$minPlayed %>% mean %>% round(., 2)
+          avg <- DT_MUSIC_BANK[beginning < Date & Date < end]$minPlayed %>% mean %>% round(., 2)
           return(c(season, avg) )
         }) 
       )  %>% as.data.table(.) %>% 
@@ -60,11 +60,19 @@ seasonal_averages <- do.call(rbind, lapply(names(seasons), function(season) {
 #'
 #'Could potentially just look at a single purchase (or type of) which could be the next step.
 #'However, I do not believe that the effort should be explored based on the initial results.
-cor(x = DT.MUSIC.BANK$Purchases, y = DT.MUSIC.BANK$minPlayed, method = 'pearson')
+cor(x = DT_MUSIC_BANK$Purchases, y = DT_MUSIC_BANK$minPlayed, method = 'pearson')
 
 #'Lag of zero represents the correlation against itself (no lag); from comparing both graphs
 #'(on the dashboard) I expected a lag of around 3~days. Based on the output below we can see that
 #'there is some significance. CROSS CORRELATION
-ccf(x=DT.MUSIC.BANK$Purchases, y= DT.MUSIC.BANK$minPlayed, lag.max = 10, type = 'correlation', plot = TRUE)
+ccf(x=DT_MUSIC_BANK$Purchases, y= DT_MUSIC_BANK$minPlayed, lag.max = 10, type = 'correlation', plot = TRUE)
+
+#'Proof that the Cross Correlation provided real results. Therefore it is not worth exploring this area any further.
+#'UNLESS we also try to consider the unique types of purchases s.a. - Food, Small Purchases, or even on the music 
+#'side where only listening in the mornings against the nights (or where one is greater than the other).
+lagged_DT_MUSIC_BANK <- cbind(DT_MUSIC_BANK[1:(NROW(DT_MUSIC_BANK)-2)]$Purchases, 
+                              DT_MUSIC_BANK$minPlayed[3:NROW(DT_MUSIC_BANK)]) %>% as.data.table
+cor(x = lagged_DT_MUSIC_BANK$V1, y = lagged_DT_MUSIC_BANK$V2, method = 'pearson') ; rm(lagged_DT_MUSIC_BANK)
+
 
 
