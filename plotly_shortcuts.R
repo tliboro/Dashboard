848e67f52bda9db2dffc54bb05eecf91be599c79
Tyler Liboro <tylerliboro@Tylers-MacBook-Pro.local>
2020-03-06 06:24:00 -0500

    Changed the display_seasons() function to accept dates based on the seasons table instead of static dates

--- plotly_shortcuts.R
+++ plotly_shortcuts.R
@@ -14,7 +14,7 @@ seasons <- list(Winter_1 = c("2019-01-01", "2019-03-18"),
                 Winter_2 = c("2019-12-01", "2019-12-31"),
                 Spring = c("2019-03-19", "2019-06-20"),
                 Summer = c("2019-06-21", "2019-09-22"),
-                Fall = c("2019-09-22", "2019-12-01"))
+                Fall = c("2019-09-22","2019-12-01" ))
 
 display_seasons <- function (fig, title, max.height) {
   output <- layout(fig, title = title, 
@@ -22,29 +22,29 @@ display_seasons <- function (fig, title, max.height) {
                      
                      #WINTER MONTHS
                      list(type = 'rect', fillcolor = "blue", line = list(color = "black"), opacity = 0.3,
-                          x0 = "2019-01-01", x1 = "2019-03-18", xref = "x",
+                          x0 = seasons$Winter_1[1], x1 = seasons$Winter_1[2], xref = "x",
                           y0 = 0, y1 = max.height, yref = "y"),
                      list(type = "rect",
                           fillcolor = "blue", line = list(color = "black"), opacity = 0.3,
-                          x0 = "2019-12-01", x1 = "2019-12-31", xref = "x",
+                          x0 = seasons$Winter_2[1], x1 = seasons$Winter_2[2], xref = "x",
                           y0 = 0, y1 = max.height, yref = "y"),
                      
                      #SPRING MONTHS
                      list(type = "rect",
                           fillcolor = "green", line = list(color = "black"), opacity = 0.3,
-                          x0 = "2019-03-19", x1 = "2019-06-20", xref = "x",
+                          x0 = seasons$Spring[1], x1 = seasons$Spring[2], xref = "x",
                           y0 = 0, y1 = max.height, yref = "y"),
                      
                      #SUMMER MONTHS
                      list(type = "rect",
                           fillcolor = "yellow", line = list(color = "black"), opacity = 0.3,
-                          x0 = "2019-06-21", x1 = "2019-09-22", xref = "x",
+                          x0 = seasons$Summer[1], x1 = seasons$Summer[2], xref = "x",
                           y0 = 0, y1 = max.height, yref = "y"),
                      
                      #FALL MONTHS
                      list(type = "rect",
                           fillcolor = "brown", line = list(color = "black"), opacity = 0.3,
-                          x0 = "2019-09-22", x1 = "2019-12-01", xref = "x",
+                          x0 = seasons$Fall[1], x1 = seasons$Fall[2], xref = "x",
                           y0 = 0, y1 = max.height, yref = "y")
                      
                    )
